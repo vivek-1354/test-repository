47b15984998d93a319bbc4892f2670cb467c8b10
vivek-1354 <108834876+vivek-1354@users.noreply.github.com>
2022-07-18 05:25:09 +0530

    comment out jwt

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import sqlalchemy
 from flask import Flask 
 from flask_restful import Api      #reqparse  # it is use to parse the payload set which data attach to the requests
-from flask_jwt import JWT
+# from flask_jwt import JWT
 
-from security import authenticate,identity 
+# from security import authenticate,identity 
 from resources.user import UserResiter,Userlist,User
 from resources.item import Item, ItemList 
 from resources.store import Store, StoreList
@@ -18,7 +18,7 @@ api = Api(app)
 """# Here we insiliaze a JWT object that is going to use our app and authenticate ,identity function together 
 to allow to authentication of the user"""
 
-jwt = JWT(app, authenticate, identity)   # Here Jwt create a endpoint called (/auth)
+# jwt = JWT(app, authenticate, identity)   # Here Jwt create a endpoint called (/auth)
 
 api.add_resource(Item, '/item/<string:name>')          #http://127.0.0.1:5000/item/name
 api.add_resource(ItemList, '/items')                   #http://127.0.0.1:5000/items
